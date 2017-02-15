a850dcd1bb9eabeb3670f2c98278552a08f265b5
robert <robertndungu16@gmail.com>
2017-02-15 08:25:09 +0300

    Added comments to the code

--- app/forms.py
+++ app/forms.py
@@ -6,13 +6,14 @@ from wtforms import BooleanField, StringField, PasswordField, validators, Submit
 from flask_pagedown.fields import PageDownField
 from wtforms.validators import DataRequired
 
+'''Login Form'''
 class LoginForm(Form):
     email = StringField('Email address', [validators.DataRequired(), validators.Email()])
     password = PasswordField('Password', [validators.DataRequired(message='Must provide a password.')])
     remember_me = BooleanField('remember_me', default=False)
     submit = SubmitField('Submit')
 
-
+'''Signup Form'''
 class SignUpForm(Form):
     firstname = StringField('firstname', [validators.Length(min=2, max=20)])
     lastname = StringField('lastname', [validators.Length(min=2, max=20)])
@@ -24,17 +25,17 @@ class SignUpForm(Form):
     ])
     confirm = PasswordField('Repeat Password')
 
-
+'''Journal Form'''
 class JournalForm(Form):
-    body = StringField('Body', [validators.Length(min=1, max=1500)])
+    body = PageDownField('Body', [validators.Length(min=1, max=1500)])
     tags = StringField('Tags', [validators.Length(min=1, max=20)])
 
-
+'''Search Form'''
 class SearchForm(Form):
     search = StringField('search', validators=[DataRequired()])
     submit = SubmitField("Submit")
 
-
+'''Edit Form'''
 class EditForm(Form):
     body = PageDownField('Body', [validators.Length(min=1, max=1500)])
     tags = StringField('Tags', [validators.Length(min=1, max=20)])
